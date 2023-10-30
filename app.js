c9aadc16bd83610b8671d2fe33d955627ce795f2
StrongTroper178 <135588642+StrongTroper178@users.noreply.github.com>
2023-10-30 16:03:06 +0200

    changed lots of things

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
 //adds required without dependencies
-var http = require('http');
-var fs = require('fs');
+const fs = require('fs');
+const http = require('http');
 
 //adds required with dependencies
+const express = require('express');
+const app = express();
 
+app.set('port', process.env.PORT || 8080);
 
-//starts http server at port 8080 (localhost)
-http.createServer(function(req, res){
-    fs.readFile('index.html', function(err, html){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(html);
-        return res.end();
-    })
-}).listen(8080);
+//creates http server called app
+const server = http.createServer(app);
+//adds socket.io dependency
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+//declare page by url
+app.get('/', (req, res) => {
+    console.log("requested page: index.html");
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/index.html', (req, res) => {
+    console.log("requested page: index.html");
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/page2.html', (req, res) => {
+    console.log("requested page: page2.html");
+  res.sendFile(__dirname + '/page2.html');
+});
+
+app.get('/signup.html', (req, res) => {
+    console.log("requested page: signup.html");
+  res.sendFile(__dirname + '/signup.html');
+});
+
+//creates connection between html and node.js
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });
+    socket.on('signup username', (username) => {
+        console.log('username: ' + username);
+    });
+    socket.on('signup password', (password) => {
+        console.log('password: ' + password);
+    });
+});
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
